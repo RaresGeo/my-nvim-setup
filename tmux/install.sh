ef74b525c08a4aa6f3ab2d2d32c19c6dfaf2e9e7
Daniel Dell XPS 15 <raresgeo9@gmail.com>
2025-11-23 15:31:54 +0200

    feat: improve installation scripts

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,21 +1,47 @@
 #!/bin/bash
+# ~/.config/nvim/tmux/install.sh
 
-# Install TPM
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# Exit on error
+set -e
+
+# Function to print messages
+log() {
+    echo -e "\e[32m$1\e[0m"
+}
+
+# Install tmux
+log "Installing tmux..."
+sudo apt update
+sudo apt install -y tmux git
+
+# Install TPM if not already installed
+if [ -d "$HOME/.tmux/plugins/tpm" ]; then
+    log "TPM is already installed at $HOME/.tmux/plugins/tpm. Skipping installation."
+else
+    log "Installing TPM (Tmux Plugin Manager)..."
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
 
 # Set up symlink
+log "Setting up .tmux.conf symlink..."
 cd ~ && ln -sf .config/nvim/tmux/.tmux.conf ~/.tmux.conf && cd -
 
-# Check if we're in a tmux session
+# Handle tmux plugin installation
 if [ -n "$TMUX" ]; then
     # We're in tmux, source the config
+    log "Sourcing tmux configuration..."
     tmux source ~/.tmux.conf
 else
     # Not in tmux, kill all existing sessions to force config reload
+    log "Killing existing tmux sessions..."
     tmux kill-server 2>/dev/null || true
 fi
 
-# Start a detached tmux session to install plugins
+# Install plugins
+log "Installing tmux plugins..."
 tmux new-session -d -s tmp_install_session
 ~/.tmux/plugins/tpm/bin/install_plugins
 tmux kill-session -t tmp_install_session
+
+log "Tmux installation completed!"
+log "Start tmux with 'tmux' or attach to existing session with 'tmux attach'."
--- zsh/install.sh
+++ zsh/install.sh
@@ -1,12 +1,48 @@
 #!/bin/bash
+# Source: https://github.com/MNMaqsood/oh-my-zsh-installer/blob/main/install_oh_my_zsh.sh
+# Exit on error
+set -e
 
-# Install oh my zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Function to print messages
+log() {
+    echo -e "\e[32m$1\e[0m"
+}
 
-# Clone plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# Install Zsh
+log "Installing Zsh..."
+sudo apt update
+sudo apt install -y zsh curl git
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# Install Oh My Zsh if not already installed
+if [ -d "$HOME/.oh-my-zsh" ]; then
+    log "Oh My Zsh is already installed at $HOME/.oh-my-zsh. Skipping installation."
+else
+    log "Installing Oh My Zsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
+
+# Install Zsh autosuggestions
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
+    log "Installing Zsh autosuggestions..."
+    git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+else
+    log "Zsh autosuggestions already installed. Skipping."
+fi
+
+# Install Zsh syntax highlighting
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]; then
+    log "Installing Zsh syntax highlighting..."
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+else
+    log "Zsh syntax highlighting already installed. Skipping."
+fi
 
 # Set up symlink
+log "Setting up .zshrc symlink..."
 cd ~ && ln -sf .config/nvim/zsh/.zshrc ~/.zshrc && cd -
+
+# Set Zsh as the default shell
+log "Setting Zsh as the default shell..."
+chsh -s $(which zsh)
+
+log "Zsh installation completed!"
