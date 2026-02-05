9f332cad36cb7659d9cff9324b16c81dde2cfd8a
Daniel Arch <raresgeo9@gmail.com>
2026-02-05 13:13:50 +0200

    feat: better git conflict finder

--- lua/plugins/git-conflict.lua
+++ lua/plugins/git-conflict.lua
@@ -20,6 +20,42 @@ return {
 		{ "<leader>gc0", "<cmd>GitConflictChooseNone<cr>",   desc = "Choose none (delete both)" },
 		{ "]x",          "<cmd>GitConflictNextConflict<cr>", desc = "Next conflict" },
 		{ "[x",          "<cmd>GitConflictPrevConflict<cr>", desc = "Previous conflict" },
-		{ "<leader>gcl", "<cmd>GitConflictListQf<cr>",       desc = "List conflicts in quickfix" },
+		{
+			"<leader>gcl",
+			function()
+				local handle = io.popen('git grep -n "^<<<<<<< " 2>/dev/null')
+				if not handle then return end
+				local result = handle:read("*a")
+				handle:close()
+
+				local entries = {}
+				local seen = {}
+				for line in result:gmatch("[^\r\n]+") do
+					local file, lnum = line:match("^(.+):(%d+):")
+					if file and lnum and not seen[file] then
+						seen[file] = true
+						table.insert(entries, string.format("%s:%s:1:", file, lnum))
+					end
+				end
+
+				if #entries == 0 then
+					vim.notify("No git conflicts found", vim.log.levels.INFO)
+					return
+				end
+
+				local conf = require("telescope.config").values
+
+				require("telescope.pickers").new({}, {
+					prompt_title = "Git Conflicts",
+					finder = require("telescope.finders").new_table({
+						results = entries,
+						entry_maker = require("telescope.make_entry").gen_from_vimgrep({}),
+					}),
+					sorter = conf.generic_sorter({}),
+					previewer = conf.grep_previewer({}),
+				}):find()
+			end,
+			desc = "List conflicts in Telescope",
+		},
 	},
 }
