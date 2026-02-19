846001d7dde581135d44885d3226b052421e4c98
Daniel Arch <raresgeo9@gmail.com>
2026-02-19 13:08:17 +0200

    feat: organize import in python

--- lsp/ruff.lua
+++ lsp/ruff.lua
@@ -11,6 +11,15 @@ return {
 	on_attach = function(client, bufnr)
 		_G.lsp_on_attach(client, bufnr)
 		client.server_capabilities.hoverProvider = false
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			buffer = bufnr,
+			callback = function()
+				client.request_sync("workspace/executeCommand", {
+					command = "ruff.applyOrganizeImports",
+					arguments = { { uri = vim.uri_from_bufnr(bufnr), version = vim.lsp.util.buf_versions[bufnr] } },
+				}, 3000, bufnr)
+			end,
+		})
 	end,
 	init_options = {
 		settings = {
