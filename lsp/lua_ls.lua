fa90d6a1a8970906215aa4fc14c63006e6540d90
Daniel Arch <raresgeo9@gmail.com>
2026-07-06 17:13:13 +0300

    fix: remove unwanted message

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -1,37 +1,36 @@
 return {
-	cmd = { 'lua-language-server' },
-	filetypes = { 'lua' },
-	root_markers = {
-		'.emmyrc.json',
-		'.luarc.json',
-		'.luarc.jsonc',
-		'.luacheckrc',
-		'.stylua.toml',
-		'stylua.toml',
-		'selene.toml',
-		'selene.yml',
-		'.git',
-	},
-	capabilities = _G.lsp_capabilities,
-	on_attach = function(client, bufnr)
-		print("attached to lua thing")
-		_G.lsp_on_attach(client, bufnr)
-	end,
-	settings = {
-		Lua = {
-			runtime = {
-				version = "LuaJIT",
-			},
-			diagnostics = {
-				globals = { "vim" },
-			},
-			workspace = {
-				library = vim.api.nvim_get_runtime_file("", true),
-				checkThirdParty = false,
-			},
-			telemetry = {
-				enable = false,
-			},
-		},
-	},
+    cmd = { 'lua-language-server' },
+    filetypes = { 'lua' },
+    root_markers = {
+        '.emmyrc.json',
+        '.luarc.json',
+        '.luarc.jsonc',
+        '.luacheckrc',
+        '.stylua.toml',
+        'stylua.toml',
+        'selene.toml',
+        'selene.yml',
+        '.git',
+    },
+    capabilities = _G.lsp_capabilities,
+    on_attach = function(client, bufnr)
+        _G.lsp_on_attach(client, bufnr)
+    end,
+    settings = {
+        Lua = {
+            runtime = {
+                version = "LuaJIT",
+            },
+            diagnostics = {
+                globals = { "vim" },
+            },
+            workspace = {
+                library = vim.api.nvim_get_runtime_file("", true),
+                checkThirdParty = false,
+            },
+            telemetry = {
+                enable = false,
+            },
+        },
+    },
 }
